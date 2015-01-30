931acbc10124eb70d2d88a7f4bbfeee38ec2bc20
Mandy Ngo <mandyngo27@gmail.com>
2015-01-30 10:05:41 -0800

    Step 9 - Completed

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -83,11 +83,5 @@ describe('PhoneCat App', function() {
     it('should display nexus-s page', function() {
       expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
     });
-    
-     it('should display 4 thumbnail image on nexus-s details page', function() {
-      var imageList = element.all(by.repeater('img in phone.images'));
-      expect(imageList.count()).toBe(4);
-    });
-   
   });
 });
